6c8812a2824fe645a8aedc82367edfd282c76dc5
Marco Vidali <marcovidali@outlook.com>
2026-05-24 14:40:07 +0200

    Add GPIO enable_pin function

--- drivers/src/gpio.rs
+++ drivers/src/gpio.rs
@@ -3,6 +3,7 @@ use core::ptr;
 use crate::PERIPHERALS_BASE_ADDR;
 
 const GPIO_BASE_ADDR: usize = PERIPHERALS_BASE_ADDR + 0x200000;
+const GPIO_PUP_PDN_BASE_ADDR: usize = GPIO_BASE_ADDR + 0xe4;
 
 #[repr(usize)]
 pub enum GPIOFunc {
@@ -33,5 +34,22 @@ pub fn set_function(pin_num: u8, func: GPIOFunc) {
         reg_val |= (func as u32) << bit_start; // set function bits to desired function
 
         ptr::write_volatile(gpfsel_ptr, reg_val); // write new GPFSEL address value
+    }
+}
+
+pub fn enable_pin(pin_num: u8) {
+    if pin_num > 57 {
+        return;
+    }
+
+    let bit_start = (pin_num % 16) * 2;
+    let reg = (pin_num / 10) as usize;
+    let target_addr = GPIO_PUP_PDN_BASE_ADDR + (reg * 4);
+    let gpio_pup_pdn_ptr = target_addr as *mut u32;
+
+    unsafe {
+        let mut reg_val = ptr::read_volatile(gpio_pup_pdn_ptr); // read entire GPIO_PUP_PDN address of the pin
+        reg_val &= !(3 << bit_start); // set pin pup_pdn to 00
+        ptr::write_volatile(gpio_pup_pdn_ptr, reg_val); // write new GPIO_PUP_PDN address value
     }
 }
